952f26d45348867e07b88dbafb263b22740838fc
Jordan Eldredge <jordan@jordaneldredge.com>
2016-02-22 12:36:11 -0800

    Install and use Underscore

--- static/js/initialize.js
+++ static/js/initialize.js
@@ -1,3 +1,8 @@
-requirejs([], function() {
-    alert('hello world');
+requirejs([
+    'underscore'
+],function(
+    _
+) {
+    var template = _.template('Hello <%- name %>');
+    document.body.innerHTML = template({name: 'Jordan'});
 });
